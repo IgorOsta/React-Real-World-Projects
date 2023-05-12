0188149a070495dd9df0e86ea53d4e64a6327d27
IgorOs <62571545+IgorOsta@users.noreply.github.com>
2023-05-12 15:29:12 +0100

    about styling

--- portfolio_website/src/components/about/About.js
+++ portfolio_website/src/components/about/About.js
@@ -3,8 +3,17 @@ import "./about.css";
 function About() {
   return (
     <div className="about">
-      <div className="about-left"></div>
-      <div className="about-right"></div>
+      <div className="about-left">
+        <div className="about-card bg"></div>
+        <div className="about-card ">
+          <img
+            src="https://ee.nd.edu/wp-content/uploads/sites/8/2020/09/grant-800x800.jpg"
+            alt="engineer in laboratory"
+            className="about-image"
+          />
+        </div>
+      </div>
+      <div className="about-right">About Right</div>
     </div>
   );
 }
