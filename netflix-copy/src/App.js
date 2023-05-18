008235b5f79396bee818d418517e335f41da0644
IgorOs <62571545+IgorOsta@users.noreply.github.com>
2023-05-18 10:05:40 +0100

    added Home component and style

--- netflix-copy/src/App.js
+++ netflix-copy/src/App.js
@@ -1,7 +1,8 @@
 import "./App.scss";
+import Home from "./home/Home";
 
 function App() {
-  return <div className="App">Netflix Copy</div>;
+  return <Home/>;
 }
 
 export default App;
---
+++ netflix-copy/src/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Netflix Home</div>;
+};
+
+export default Home;
