a982ddc89c0ae1a323dc9b3caaefdd5fe194bf1d
IgorOs <62571545+IgorOsta@users.noreply.github.com>
2023-05-18 11:15:41 +0100

    NavBar added changes

--- netflix-copy/src/components/NavBar.jsx
+++ netflix-copy/src/components/NavBar.jsx
@@ -1,17 +1,31 @@
 import "./navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import PersonIcon from "@mui/icons-material/Person";
 const NavBar = () => {
   return (
     <div className="navBar">
       <div className="container">
         <div className="left">
-          <img src="" alt="" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg"
+            alt=""
+          />
           <span>Homepage</span>
           <span>Series</span>
           <span>Movies</span>
           <span>New and Popular</span>
           <span>My List</span>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <SearchIcon />
+          <span>KID</span>
+          <NotificationsIcon />
+
+          <ArrowDropDownIcon />
+          <PersonIcon />
+        </div>
       </div>
     </div>
   );
