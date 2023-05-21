129a5842fc51a0f62880802ed876ed5e97b88b49
IgorOs <62571545+IgorOsta@users.noreply.github.com>
2023-05-21 18:43:06 +0100

    Connected to MongoDB

--- mernStackNetflix/api/index.js
+++ mernStackNetflix/api/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(
+    "mongodb+srv://igor:igor@cluster0.svotqiv.mongodb.net/netflix?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB connection successful"));
 
 app.listen(8800, () => {
   console.log("Backend server is running !");
