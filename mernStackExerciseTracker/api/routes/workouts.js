9cc5b000d0c177ba363796a781c720e3f6fddb06
IgorOs <62571545+IgorOsta@users.noreply.github.com>
2023-06-01 06:14:31 +0100

    Delete and Update controllers

--- mernStackExerciseTracker/api/routes/workouts.js
+++ mernStackExerciseTracker/api/routes/workouts.js
@@ -1,36 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workoutModel");
+const {
+  createNewWorkout,
+  getWorkouts,
+  getSingleWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutControllers");
 
 //* GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: " GET all workouts " });
-});
+router.get("/", getWorkouts);
 
 //* GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ mssg: " GET a single workout " });
-});
+router.get("/:id", getSingleWorkout);
 //* POST a new workout
-router.post("/", async (req, res) => {
-  const { title, reps, load } = req.body;
-  console.log(title, reps, load);
-  try {
-    //* Workout .create is asynchronous
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createNewWorkout);
 //* DELETE a workout
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: " DELETES the selected workout " });
-});
+router.delete("/:id", deleteWorkout);
 
 //* UPDATE a workout
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: " UPDATES selected workout " });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
