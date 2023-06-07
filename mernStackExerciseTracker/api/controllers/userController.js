5eb9f1b0475e1579cc0650934b2f7c34a4d71d03
IgorOs <62571545+IgorOsta@users.noreply.github.com>
2023-06-07 04:32:10 +0100

    JSON web token backend

--- mernStackExerciseTracker/api/controllers/userController.js
+++ mernStackExerciseTracker/api/controllers/userController.js
@@ -1,5 +1,12 @@
 const Workout = require("../models/workoutModel");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken")
+
+//* Create Token function
+
+const createToken = (_id) => {
+ return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //*Login user
 async function loginUser(req, res) {
@@ -8,7 +15,14 @@ async function loginUser(req, res) {
 
 //*Signup user
 async function signUpUser(req, res) {
-  res.status(200).json({ msg: "User Signed Up..." });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.json({ msg: error.message });
+  }
+  res.json({ msg: "Signed up user " });
 }
 
 module.exports = { loginUser, signUpUser };
--- mernStackExerciseTracker/api/models/userModel.js
+++ mernStackExerciseTracker/api/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
   email: {
@@ -14,13 +15,30 @@ const userSchema = mongoose.Schema({
 });
 
 //! We are creating our own static method SignUp
-userSchema.statics.signup = async (email, password) => {
+userSchema.statics.signup = async function (email, password) {
+  //* Validation
+  //*Check if we have a value for email and password.
+
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  //* Check if the email is a valid one.
+  if (!validator.isEmail(email)) {
+    throw Error("Please enter a valid email");
+  }
+  //*Check if the password is a strong one, uppercase, lowercase, minimum length
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
   const exists = await this.findOne({ email });
   if (exists) {
     throw Error("Email already in use");
   }
   const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password,salt);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+  return user;
 };
 
 module.exports = mongoose.model("User", userSchema);
